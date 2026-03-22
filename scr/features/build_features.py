254753a662fad9c1aade5bbaf3aff0c12972f0d3
Daksanna <55077781+Daksanna@users.noreply.github.com>
2026-03-22 10:18:40 +0100

    Delete scr directory

--- scr/features/build_features.py
+++
@@ -1,2 +0,0 @@
-def aggregate_features(df):
-    return df.groupby("grid_id").mean().reset_index()
--- scr/features/indices.py
+++
@@ -1,5 +0,0 @@
-def compute_ndvi(nir, red):
-    return (nir - red) / (nir + red)
-
-def compute_ndbi(swir, nir):
-    return (swir - nir) / (swir + nir)
